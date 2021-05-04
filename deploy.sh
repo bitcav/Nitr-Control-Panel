fe3c93d46c65528df753f2faf27d6ef0003dc49a
Juan Huttemann <juanfhuttemann@gmail.com>
2021-05-03 23:52:59 -0400

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd NitrControlPanel
 dotnet build --configuration Release
-dotnet publish -c Release -p:PublishSingleFile=true --runtime win-x64 -o Publish
+dotnet publish -c Release -p:PublishSingleFile=true --runtime win-x64 --self-contained false -o Publish
